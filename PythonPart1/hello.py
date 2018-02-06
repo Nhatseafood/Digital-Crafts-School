40d491ff2fd8ffc3dc6a76ad0299882622ba7d66
Nhatchet <NhaNguyen@GTC3-MBP.localdomain>
2018-02-06 10:34:05 -0600

    testing push

--- PythonPart1/hello.py
+++ PythonPart1/hello.py
@@ -1,3 +1,19 @@
-
+'''
+#problem 1
 name = input('What is your name? ')
 print("Hello," +name +"!")
+#problem 2
+name = input('What is your name? ')
+print("Hello," +name +"!")
+print("Your Name Has", len(name), "LETTER IN IT! AWESOME!")
+#problem 3
+#input ("name", "subject")
+name = input("What is your name")
+subject = input("What is the subject")
+print(name + "'s favorite subject in school is" + subject)
+'''
+#problem 4
+#day = int(input('Day (0-6)? '))
+week = ["Sunday", "Monday", "Tuesday", "Wednesday," "Thuesday", "Friday", "Sunday"]
+day_of_week =int(input("Please give day of the week (0-6) 0 is Sunday."))
+print(week[day_of_week])
