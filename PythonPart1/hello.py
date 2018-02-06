431afb5a034b66da7fccab170337ed966609c26e
Nhatchet <NhaNguyen@GTC3-MBP.localdomain>
2018-02-06 11:07:48 -0600

    Work or sleep

--- PythonPart1/hello.py
+++ PythonPart1/hello.py
@@ -11,9 +11,21 @@ print("Your Name Has", len(name), "LETTER IN IT! AWESOME!")
 name = input("What is your name")
 subject = input("What is the subject")
 print(name + "'s favorite subject in school is" + subject)
-'''
+
 #problem 4
 #day = int(input('Day (0-6)? '))
 week = ["Sunday", "Monday", "Tuesday", "Wednesday," "Thuesday", "Friday", "Sunday"]
 day_of_week =int(input("Please give day of the week (0-6) 0 is Sunday."))
 print(week[day_of_week])
+'''
+#problem 5
+week = ["Sunday", "Monday", "Tuesday", "Wednesday," "Thuesday", "Friday", "Sunday"]
+day_of_week =int(input("Please give day of the week (0-6) 0 is Sunday."))
+if day_of_week == 0 or day_of_week ==6:
+    print("Sleep in")
+
+if day_of_week >=1 and day_of_week<=5:
+    print("Go to work")
+
+
+       
