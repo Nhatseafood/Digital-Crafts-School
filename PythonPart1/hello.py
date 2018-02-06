9525228352476814176e7a986fd8352a1bf7ef95
Nhatchet <NhaNguyen@GTC3-MBP.localdomain>
2018-02-06 12:04:57 -0600

    Tips and total

--- PythonPart1/hello.py
+++ PythonPart1/hello.py
@@ -17,7 +17,7 @@ print(name + "'s favorite subject in school is" + subject)
 week = ["Sunday", "Monday", "Tuesday", "Wednesday," "Thuesday", "Friday", "Sunday"]
 day_of_week =int(input("Please give day of the week (0-6) 0 is Sunday."))
 print(week[day_of_week])
-'''
+
 #problem 5
 week = ["Sunday", "Monday", "Tuesday", "Wednesday," "Thuesday", "Friday", "Sunday"]
 day_of_week =int(input("Please give day of the week (0-6) 0 is Sunday."))
@@ -31,6 +31,22 @@ if day_of_week >=1 and day_of_week<=5:
 tempc = int(input("Tempature in C")) 
 tempf = tempc * 1.8 + 32
 print(tempf)
-
+'''
+#problem 7
+total = int(input("What is the total bill"))
+rating = input("level of service? [put good, fair, or bad]")
+level_of_service = ("good", "fair", "bad")
+if rating == ("good"): 
+   tip = total *.2
+   total = total + tip
+   print(total)
+elif rating == "fair":
+    tip = total *.15
+    total = total + tip
+    print(total)
+else:
+    tip = total *.1
+    total = total + tip
+    print(total)
 
        
