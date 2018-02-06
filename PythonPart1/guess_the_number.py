32f1087a8f0d49d5d5c3e0c221192e48beffe2d7
Nhatchet <NhaNguyen@GTC3-MBP.localdomain>
2018-02-06 15:36:39 -0600

    guessing2

--- PythonPart1/guess_the_number.py
+++ PythonPart1/guess_the_number.py
@@ -1,3 +1,4 @@
+'''
 secret_number = 5
 print("I am thinking of a number between 1 and 10.")
 while True:
@@ -7,5 +8,17 @@ while True:
     else:
         print("Yes! You Win! ")
         break
-
+'''
+#Step 2
+secret_number = 5
+print("I am thinking of a number between 1 and 10.")
+while True:
+    total = input("What is the number? ")
+    if total > '5':
+        print(total + "  is too high")
+    elif total < '5':
+        print(total + " is too low")
+    else:
+        print("Yes! You Win! ")
+        break
 
