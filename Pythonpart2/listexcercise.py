57d983262271394d9ae88acbf7e134d166236474
Nhatchet <NhaNguyen@GTC3-MBP.localdomain>
2018-02-07 12:18:49 -0600

    positivenumber2

--- Pythonpart2/listexcercise.py
+++ Pythonpart2/listexcercise.py
@@ -6,7 +6,30 @@ print(sum(number))
 #largestnum
 number = [1, 2, 3, 100]
 print(max(number))
-'''
+
 #smallestnum
 number = [4, 5, 6, 7]
 print(min(number))
+
+
+#evennumbers
+a = [1, 2, 3, 4]
+for number in a:
+    if number %2 ==0:
+        print(number)
+
+
+# 5
+a = [1, 2, 3, 4, 5]
+for i in a:
+    if i > 0:
+        print(i)   
+'''
+#6
+a = [1, 2, 3, 4, 5]
+b=[]
+for i in a:
+    if i > 0:
+        b.extend([i])
+
+print(b)          
