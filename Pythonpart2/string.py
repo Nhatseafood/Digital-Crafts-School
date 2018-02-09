32557f68b97699278abcfbca885d190e7c965f10
Nhatchet <NhaNguyen@GTC3-MBP.localdomain>
2018-02-09 14:23:52 -0600

    Leetspeak

--- Pythonpart2/string.py
+++ Pythonpart2/string.py
@@ -4,7 +4,33 @@
 name = "Nhat"
 print(name.upper())
 
-'''
 # 2
 name = "nhat"
 print(name.capitalize())
+
+'''
+# 3
+name = "Nhat"
+print(name[::-1])
+
+#4
+paragraph = "Hello my name is Nhat".upper()
+leet = ''
+for i in paragraph:
+    if i == "A":
+        leet += '4'
+    elif i == "E":
+        leet += '3'
+    elif i == "G":
+        leet += '6'
+    elif i == "I":
+        leet += '1'
+    elif i == "O":
+        leet += '0'
+    elif i == "S":
+        leet += '5'
+    elif i == "T":
+        leet += '7'
+    else:
+        leet += i
+print(leet)
