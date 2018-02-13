400252ed0387d5b15db7de7a6e6af93a8806bbea
Nhatchet <NhaNguyen@GTC3-MacBook-Pro.local>
2018-02-13 14:27:13 -0600

    str

--- Week2/objects.py
+++ Week2/objects.py
@@ -18,7 +18,6 @@ sonny.greet(jordan)
 jordan.greet(sonny)
 print('Contact info, Name {}, Email {},Phone {}'.format(sonny.name, sonny.email, sonny.phone))
 
-'''
 
 #2
 
@@ -30,6 +29,8 @@ class Person:
         self.phone = phone
         self.friend = []
         self.greeting_count = 0
+
+    
        
  
     def print_contact_info(self):
@@ -54,6 +55,58 @@ jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456')
 sonny.print_contact_info()
 sonny.num_friend()
 sonny.greeting_count
+
+
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+    def print_info(self):
+        print(self.year, self.make, self.model)
+
+car = Vehicle('Nissan', 'leaf', '2015')
+car.print_info()
+'''
+
+#3
+class Person:
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.friend = []
+        self.greeting_count = 0
+
+    def __str__(self):
+       return'Person: {} {} {}'.format(self.name, self.email, self.phone)
+
+    
+       
+ 
+    def print_contact_info(self):
+        print('email:' ,self.email, 'phone number:' ,self.phone)
+    def add_friend(self, x):
+        self.friend.append(x)
+    def num_friend(self):
+        print(len(self.friend))
+
+
+    def greet(self, other_person):
+        print('Hello {}, I am {}!'.format(other_person.name, self.name))
+    
+    def greeting_count(self):
+        print(greet)
+        self.greeting_count += 1
+
+
+sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948')
+jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456')
+
+sonny.print_contact_info()
+sonny.num_friend()
+sonny.greeting_count
+print(sonny)
 
 
 class Vehicle:
