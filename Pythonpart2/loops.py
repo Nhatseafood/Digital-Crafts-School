c360a653c7274e09f7b4837688313861d3de4db9
Nhatchet <NhaNguyen@GTC3-MBP.localdomain>
2018-02-14 14:35:59 -0600

    sqaure2

--- Pythonpart2/loops.py
+++ Pythonpart2/loops.py
@@ -4,7 +4,23 @@
 for number in range(1,11):
     print(number)
 
-'''
 #2
-for number in range(2,8):
+start = int(input('Start from: '))
+end = int(input('End from: '))
+for number in range(start,end + 1):
     print(number)
+    
+#3
+for number in range(1,11):
+    if number %2 != 0:
+        print(number)
+
+#4
+size = 5
+for i in range(size):
+    print('*' * size)
+'''
+#5
+square = int(input("How big is the square? "))
+for number in range(square):
+    print('*' * square)
