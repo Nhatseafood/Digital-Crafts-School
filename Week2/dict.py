57e431ffec59f8eec944b6cb099debd1b425c17a
Nhatchet <NhaNguyen@GTC3-MBP.localdomain>
2018-02-15 11:08:42 -0600

    dict2

--- Week2/dict.py
+++ Week2/dict.py
@@ -1,4 +1,6 @@
 
+'''
+#1
 phonebook_dict = {
     'Alice' : '703-493-1834' ,
     'Bob' : '857-384-1234' ,
@@ -12,3 +14,29 @@ print(phonebook_dict['Kareem'])
 del phonebook_dict['Alice']
 phonebook_dict['Bob'] = '938-3452-2345'
 print(phonebook_dict)
+
+'''
+#2
+
+ramit = {
+  'name': 'Ramit',
+  'email': 'ramit@gmail.com',
+  'interests': ['movies', 'tennis'],
+  'friends': [
+    {
+      'name': 'Jasmine',
+      'email': 'jasmine@yahoo.com',
+      'interests': ['photography', 'tennis']
+    },
+    {
+      'name': 'Jan',
+      'email': 'jan@hotmail.com',
+      'interests': ['movies', 'tv']
+    }
+  ]
+}
+
+print(ramit['email'])
+print(ramit['interests'][0])
+print(ramit['friends'][0]['email'])
+print(ramit['friends'][1]['interests'][1])
